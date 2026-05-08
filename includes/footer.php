a3ac8cfd0e7d2c5d37cbba4270f57ff2184a7fad
Rika600 <karima740@icloud.com>
2026-05-08 11:51:05 +0200

    fix: Bootstrap JS et Icons chargés via CDN au lieu de node_modules

--- includes/footer.php
+++ includes/footer.php
@@ -17,7 +17,7 @@
     </div>
 </footer>
 
-<script src="<?= BASE_URL ?>node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
 </html>
